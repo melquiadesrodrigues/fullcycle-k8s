8826e450d0cb96129c69216eb2f89793213bcabf
Melquíades <melquiades.rodrigues@hotmail.com>
2021-05-13 18:42:59 -0300

    deployment with envs

--- application/server.go
+++ application/server.go
@@ -1,12 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.HandleFunc("/config", ConfigMap)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
