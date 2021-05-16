648201f3404067f3d6d1abac99cd9e0bc4dbe40e
Melquíades <melquiades.rodrigues@hotmail.com>
2021-05-15 23:12:30 -0300

    deployment with persistent volumes

--- application/server.go
+++ application/server.go
@@ -6,11 +6,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/config", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -28,4 +33,25 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "My Family: %s.", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("USER")
+	age := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s. Password: %s", name, age)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("Ok"))
+	}
+
 }
